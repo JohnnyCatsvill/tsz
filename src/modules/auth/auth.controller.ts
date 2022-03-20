ea6c8d9a24be2504208c5842bb7bed3b34b49500
ImperialMan <1>
2022-03-20 19:56:37 +0300

    авторизацию начал делать но она не работает

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,16 +3,16 @@ import { AuthService } from './auth.service';
 import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 import { ApiTags } from "@nestjs/swagger";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
+import { LoginDto } from "./dto/login.dto";
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() request) {
-    return this.authService.login(request.user);
+  async login(@Body() loginDto: LoginDto) {
+    //return this.authService.login(request.user);
   }
 
   // @Post('register')
